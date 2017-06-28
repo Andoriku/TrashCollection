121117d2fbc15371e002788d5daf751691d8c431
Andrew Jordan <andoriku@gmail.com>
2017-06-28 17:27:15 -0500

    initial commit/login in information

---
+++ Trash Collecter Main/Trash Collecter Main/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Trash_Collecter_Main.Startup))]
+namespace Trash_Collecter_Main
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
