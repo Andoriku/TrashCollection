6d00227c81364807dabfb93f25a59ec693dbfed3
Andrew Jordan <andoriku@gmail.com>
2017-06-30 15:55:44 -0500

    Added the right file

--- Trash Collecter Main/Trash Collecter Main/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Trash_Collecter_Main.MvcApplication" Language="C#" %>
---
+++ Trash Collector/Trash Collector/Migrations/Configuration.cs
@@ -0,0 +1,42 @@
+namespace Trash_Collector.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Trash_Collector.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Trash_Collector.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            context.Roles.AddOrUpdate(r => r.Name,
+
+               new IdentityRole { Name = "Customer" },
+               new IdentityRole { Name = "Employee" },
+               new IdentityRole { Name = "Admin" }
+               );
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            UserManager.AddToRole("035ede2b-3986-4173-84f7-648ebb8d870a", "Admin");
+        }
+    }
+}
